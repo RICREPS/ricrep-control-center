c83f0e1c16d8d673fde20ae72081dbeb0a694f63
RICREPS <josedavidberti@ricrep.com>
2026-06-28 14:05:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
 
-  FLYCAR_BASE_URL: "
-https://bots.apps.ispcore.es/webhook/49d5653d-68a5-4ce5-a6e0-livemap?access=iH99CuXb4cg5aPLhUGSxvvSeRhGUcnZEyVf8dCfKT9YvLCAEtwedMQVnGV54z7DLLh8V4F0lCKoAYCUI6NeG0b8u0OP0gS43SbO&fleet_id=RICREP",
+  FLYCAR_BASE_URL: "https://bots.apps.ispcore.es/webhook/49d5653d-68a5-4ce5-a6e0-livemap?access=iH99CuXb4cg5aPLhUGSxvvSeRhGUcnZEyVf8dCfKT9YvLCAEtwedMQVnGV54z7DLLh8V4F0lCKoAYCUI6NeG0b8u0OP0gS43SbO&fleet_id=RICREP",
 
   CITY_IDS: [
     1016,
